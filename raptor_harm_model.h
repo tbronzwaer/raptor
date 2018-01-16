c0515b245c9690a1548ff0b1163d964db12a86ae
Jordy <jordydavelaar@gmail.com>
2018-01-16 13:25:08 +0100

    cleaned up

--- raptor_harm_model.h
+++ raptor_harm_model.h
@@ -16,19 +16,8 @@
 #include "parameters.h"
 #include "functions.h"
 
-double ****econ;
-double ****ecov;
-double ***bcon;
-double ***bcov;
-double ***ucon;
-double ***ucov;
-double **ne;
-double **thetae;
-double **b;
-//double ***p;
 
 int n1, n2, n3;
-int n_within_horizon;
 
 double Ladv, dMact;
 
@@ -44,30 +33,7 @@ double Ladv, dMact;
 
 #define TP_OVER_TE    (0.)
 
-/* some coordinate parameters */
-extern real R0, Rin, Rh, Rout, Rms;
-extern real a;
-extern real hslope;
-extern real startx[NDIM], stopx[NDIM], dx[NDIM];
-extern real dlE, lE0;
-extern real gam;
-extern real dMsim;
-
-//double M_unit;
-extern real L_unit;
-extern real T_unit;
-extern real RHO_unit;
-extern real U_unit;
-extern real B_unit;
-extern real Ne_unit;
-extern real Thetae_unit;
-
 #pragma acc copyin(startx,stopx,dx,Thetae_unit,Ne_unit,B_unit,U_unit,RHO_unit,T_unit,L_unit,R0,Rin,Rh,Rout,Rms,a,hslope)
-
-#pragma acc routine (metric_dd_2)
-void metric_dd_2(double xp[NDIM], double gcov[NDIM][NDIM]);
-#pragma acc routine (metric_uu_2)
-void metric_uu_2(double xp[NDIM], double gcon[NDIM][NDIM]);
 
 // RAPTOR_HARM_MODEL.C
 //////////////////////
