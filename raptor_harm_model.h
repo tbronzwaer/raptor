4034004d3d6d3a98998b72272bb5aea7e4a6a1a5
Thomas <t.bronzwaer@astro.ru.nl>
2018-01-29 13:30:41 +0100

    Added license for GRMONTY stuff

--- raptor_harm_model.h
+++ raptor_harm_model.h
@@ -7,7 +7,49 @@
  * GRMONTY is released under the GNU GENERAL PUBLIC LICENSE.
  * Modifications were made in August 2016 by T. Bronzwaer and
  * J. Davelaar.
+ *
+ * A copy of the GRMONTY license is included below.
  */
+
+/***********************************************************************************
+    Copyright 2013 Joshua C. Dolence, Charles F. Gammie, Monika Mo\'scibrodzka,
+                   and Po Kin Leung
+
+                        GRMONTY  version 1.0   (released February 1, 2013)
+
+    This file is part of GRMONTY.  GRMONTY v1.0 is a program that calculates the
+    emergent spectrum from a model using a Monte Carlo technique.
+
+    This version of GRMONTY is configured to use input files from the HARM code
+    available on the same site.   It assumes that the source is a plasma near a
+    black hole described by Kerr-Schild coordinates that radiates via thermal
+    synchrotron and inverse compton scattering.
+
+    You are morally obligated to cite the following paper in any
+    scientific literature that results from use of any part of GRMONTY:
+
+    Dolence, J.C., Gammie, C.F., Mo\'scibrodzka, M., \& Leung, P.-K. 2009,
+        Astrophysical Journal Supplement, 184, 387
+
+    Further, we strongly encourage you to obtain the latest version of
+    GRMONTY directly from our distribution website:
+    http://rainman.astro.illinois.edu/codelib/
+
+    GRMONTY is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GRMONTY is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GRMONTY; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+***********************************************************************************/
 
 #ifndef RAPTOR_HARM_MODEL_H
 #define RAPTOR_HARM_MODEL_H
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-make harm GPU=1
+make harm CPU=1
 echo ' '
 ./RAPTOR model.in dump040 1e21 60 3 3 1
 echo ''
